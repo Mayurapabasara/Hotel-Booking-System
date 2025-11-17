e0899658f2ee20e5681da046cb2bbff5069eb45a
MayuraPabasara <mayurapabasara143@gmail.com>
2025-11-17 22:19:01 +0530

    ixed the critical error in 'handleRoomInputChange' by implementing correct price parsing (using parseFloat) and resolved the typo in 'handleSubmit' by calling the utility 'addRoom' function instead of the component name.

--- lakeside-hotel/src/components/room/AddRoom.jsx
+++ lakeside-hotel/src/components/room/AddRoom.jsx
@@ -57,10 +57,70 @@ const AddRoom = () => {
 
   return (
     <>
-    <section className="container, mt-5"></section>
-        <div>
-            <div>
-                <h2>Add a New Room</h2>
+    <section className="container, mt-5 mb-5"></section>
+        <div className="row justify-content-center">
+            <div className='col-md-8 col-lg-6'>
+                <h2 className='mt-5 mb-2'>Add a New Room</h2>
+                <form onSubmit={handleSubmit}>
+
+                    <div className='mb-3'>
+                        <label htmlFor="roomType" className="form-label"> 
+                            Room Type 
+                        </label>
+                        <div> </div>
+                    </div>
+
+
+                    <div className='mb-3'>
+                        <label htmlFor="roomPrice" className="form-label"> 
+                            Room Price 
+                        </label>
+                        <input 
+                            className='form-control'
+                            required
+                            id="roomPrice"
+                            type="number"
+                            name="roomPrice"
+                            value="{newRoom.roomPrice}"
+                            onChange={handleRoomInputChange}
+                        />
+                    </div>
+
+
+                    <div className='mb-3'>
+                        <label htmlFor="roomPhoto" className="form-label"> 
+                            Room Photo 
+                        </label>
+
+                        <input
+                            id="photo"
+                            name="photo"
+                            type="file"
+                            className="form-control"
+                            onChange={handleImageChange}
+                        />
+
+                        {imagePreview && (
+                            <img src={imagePreview}
+                            alt="Preview Room Photo"
+                            style={{maxWidth: "400px", maxHeight: "400px"}}
+                            className="mb-3"/>
+                        )}
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+                </form>
+
             </div>
         
     </div>
